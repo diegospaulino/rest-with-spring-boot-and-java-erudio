47c386728b30017881fc3caa6b97ca5c282490a4
diegospaulino <diegopaulino023@gmail.com>
2023-12-13 19:29:33 -0300

    Teste troca de branches

--- 05_first_steps_in_java_with_spring_boot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/GreentingController.java
+++ 05_first_steps_in_java_with_spring_boot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/GreentingController.java
@@ -15,5 +15,6 @@ public class GreentingController {
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		
 	}
 }
