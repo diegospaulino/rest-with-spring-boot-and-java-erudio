7e1695a65dbb6bfb957e690b3f68f6fab2228c48
diegospaulino <diegopaulino023@gmail.com>
2023-12-29 20:39:52 -0300

    11_Adicionando_Suporte_a_Migrations

---
+++ 06_ParametersAndExceptionsHandler/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES 
+	(1,'São Paulo','Ayrton','Male','Sena'),
+	(2,'Anchiano - Italy','Leonardo','Male','da Vinci'),
+	(4,'Porbandar - India','Indira','Female','Gandhi'),
+	(5,'Porbandar - India','Mahatma','Male','Gandhi'),
+	(7,'Kentucky - US','Muhammad','Male','Ali'),
+	(9,'Mvezo - South Africa','Nelson','Male','Mandela'),
+	(10,'Smiljan - Croatia','Nikola','Male','Tesla');
